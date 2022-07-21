019e6a24154a4897dabd3f86d0f491b19cd1c3c1
Nobita <58627889+Nobita22@users.noreply.github.com>
2022-07-21 21:17:02 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,3 @@
-importScripts(
-    'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
-);
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register("/serviceworker.js");
+    navigator.serviceWorker.register('/service-worker.js');
 }
